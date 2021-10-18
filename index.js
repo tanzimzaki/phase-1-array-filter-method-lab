48c24ae3f70e066cb9f0ef2f0d72e3ae321e73e0
tanzimzaki <tanzimzaki@gmail.com>
2021-10-18 22:36:58 +0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+const cities = [
+    {name: 'Sydney', population: 3502021},
+    {name: 'Brisbane', population: 817000},
+    {name: 'Melbourne', population: 200000},
+    {name: 'Perth', population: 2006751},
+    {name: 'Hobart', population: 1226000}
+];
+
+
+// Array filter() in a function shown below
+let megaCities = cities.filter(function (answer) {
+    return answer.population > 3000000;
+});
+console.log(megaCities);
